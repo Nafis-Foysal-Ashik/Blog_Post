6b6c506cf61c56f4f583d32cdc0eb0d074163860
Nafis-Foysal-Ashik <nafisfoisal25@gmail.com>
2025-06-19 16:37:09 +0600

    config.js auth.js conf.js functionality added

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <h1>A blog post appp with AppWrite</h1>
+    </>
+  )
+}
+
+export default App
